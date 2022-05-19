74cd48cdbf118aea4dc6eafa05eeb633a6db515e
sam <wenghaishi1@gmail.com>
2022-05-19 13:42:36 +0800

    test

--- Hangman.py
+++ Hangman.py
@@ -1,3 +1,26 @@
 from Words import words
 
-print(words)
+def get_valid_word(word):
+    word = random.choice(words) 
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+    return word.upper()
+
+def hangman():
+    word = get_valid_word(word)
+    word_letters = set(word)
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()
+
+    user_letter = input('guess a letter').upper()
+    if user_letter in alphabet - used_letter:
+        used_letters.add(user_letters)
+        if user_letters in  word_letters:
+            word_letters.remove(user_letter)
+
+el
+
+
+
+user_input = input('guess a letter, type something')
+
